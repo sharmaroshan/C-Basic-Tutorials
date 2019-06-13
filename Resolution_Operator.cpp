016ff04ce37a3fa19b7a74bdef9adf24da98d42d
Roshan Sharma <37844689+sharmaroshan@users.noreply.github.com>
2019-06-13 13:37:17 +0530

    Create Resolution_Operator.cpp

---
+++ Resolution_Operator.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<conio.h>
+using namespace std; 
+
+class Name 
+{ 
+    public: 
+    string name; 
+    int id; 
+    void printname(); 
+    void printid() 
+    { 
+        cout << "Geek id is: " << id; 
+    } 
+}; 
+  
+// Definition of printname using scope resolution operator :: 
+void Name::printname() 
+{ 
+    cout << "Geekname is: " << geekname;  
+} 
+int main() { 
+      
+    Name obj1; 
+    obj1.name = "Roshan"; 
+    obj1.id=15; 
+      
+    obj1.printname(); 
+    cout << endl;  
+    obj1.printid(); 
+    return 0; 
+} 
