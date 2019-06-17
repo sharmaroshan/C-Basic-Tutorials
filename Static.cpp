fca24fe8cafbf7e4874f2a39c2059d8cfd30fa0f
Roshan Sharma <37844689+sharmaroshan@users.noreply.github.com>
2019-06-17 13:43:22 +0530

    Create Static.cpp

---
+++ Static.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+class Number
+{
+   int x;
+   static int A;
+   public:
+    void get()
+    {}
+   
+   static void show()
+    {a}
+    void show(){x}
+    
+ };
+ int Number:: a = 10;
+ int main()
+ {
+  Number no1, no2, no3;
+  
+  Number:: show();
+  }
