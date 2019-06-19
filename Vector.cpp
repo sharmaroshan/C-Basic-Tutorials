9094f88d609a667bcfcb7ed076604218a770b813
Roshan Sharma <37844689+sharmaroshan@users.noreply.github.com>
2019-06-19 15:52:26 +0530

    Create Vector.cpp

---
+++ Vector.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main()
+{
+  vactor<int>v1;
+  int a = 10;
+  int b = 20;
+  v1.push_back(a);
+  v1.push_back(b);
+  v1.push_back(101);
+  v1.push_back(202);
+  
+  for(vector<int>::iterator itr = v1.begin(); itr!= v1.end(); ++itr)
+      cout<<*itr<<" ";
+     
+  v1.pop_back();
+  cout<<"\n After pop back ";
+  for(vector<int>::iterator itr = v1.begin(); itr!= v1.end(); ++itr)
+      cout<<*itr<<" ";
+}
